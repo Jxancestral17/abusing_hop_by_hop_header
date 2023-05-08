3319c98ea53387ede1e73a563d4b7816bce2d160
Federico Manfredi Longo <federicomanfredi.longo@gmail.com>
2023-05-08 11:47:11 +0200

    Fine

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/Jxancestral17/abusing_hop_by_hop_header/headers"
 )
@@ -11,20 +12,20 @@ func main() {
 	headers.Url = os.Args[1]
 	fmt.Println("[+]Read headers file")
 	headers.Headers = headers.Readfile()
-	// for i := 0; i < len(headers.Headers); i++ {
+	for i := 0; i < len(headers.Headers); i++ {
 
-	// 	headers.Params1 = headers.GeneretorRandomString()
-	// 	headers.Params2 = headers.GeneretorRandomString()
+		headers.Params1 = headers.GeneretorRandomString()
+		headers.Params2 = headers.GeneretorRandomString()
 
-	// 	resp1 := headers.MakeRequestNoHeaders()
-	// 	resp2 := headers.MakeRequestWithHeaders(headers.Headers[i])
+		resp1 := headers.MakeRequestNoHeaders()
+		resp2 := headers.MakeRequestWithHeaders(headers.Headers[i])
 
-	// 	if resp1 != nil && resp2 != nil {
-	// 		headers.Check(resp1, resp2)
-	// 	} else {
-	// 		fmt.Println("[-]No poisoning detected")
-	// 	}
-	// 	time.Sleep(time.Millisecond * 300)
-	// }
+		if resp1 != nil && resp2 != nil {
+			headers.Check(resp1, resp2, headers.Headers[i])
+		} else {
+			fmt.Println("[-]No poisoning detected")
+		}
+		time.Sleep(time.Millisecond * 300)
+	}
 
 }
